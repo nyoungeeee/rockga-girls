6e20950a23dd306d43e80482bc17221a4c9012f4
nyoungeeee <62043951+nyoungeeee@users.noreply.github.com>
2026-08-17 12:04:28 +0900

    Add 26 JUMF open chat room

--- data.js
+++ data.js
@@ -4,7 +4,7 @@
  */
 export const ARCHIVE_DATA = {
   site: {
-    lastUpdated: "2026.08.14",
+    lastUpdated: "2026.08.17",
     submitUrl: "https://open.kakao.com/o/gceOSMIi",
   },
 
@@ -125,6 +125,7 @@ items: [
   ],
 
   rooms: [
+    { name: "26 JUMF 공유방", description: "", url: "https://open.kakao.com/o/g5DgYeJi", note: "", active: true },
     { name: "운동하는 여자들 🏃🏃‍♀️🏃‍♂️", description: "작은 일에도 부둥부둥 해드립니다", url: "https://open.kakao.com/o/g8OMTZvh", note: "복복복🫳🫳🫳", active: true },
     { name: "캠핑 혼자가는 여자들", description: "락가여 파생 캠핑방", url: "https://open.kakao.com/o/gMYasvIi", note: "입장 시 닉네임 / 지역 / 선호캠핑종류 입력", active: true },
   ],
